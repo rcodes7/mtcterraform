b8805f6173e769fc3e875a3876e5a0c5a858936f
raul <123352622+rcodes7@users.noreply.github.com>
2023-01-30 22:47:32 -0600

    create resource nodered_image

---
+++ terraform-docker/main.tf
@@ -0,0 +1,16 @@
+// https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs
+terraform {
+  required_providers {
+    docker = { // this is an alias that says that any resource that starts with docker, will use this provider
+      source  = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+  }
+}
+
+// leave empty since it is a local provider
+provider "docker" {}
+
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
