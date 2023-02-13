33b0b92ccc2c41567fe283d4330e6e18e26f704d
raul <123352622+rcodes7@users.noreply.github.com>
2023-02-12 23:11:24 -0600

    add outputs for ip address and container name

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -23,3 +23,19 @@ resource "docker_container" "nodered_container" {
     external = 1880
   }
 }
+
+
+#View them with (name must not contain spaces):
+#- terraform output
+#- terraform console > provide path of attribute youd like to see
+#- terraform show | grep ....
+
+output "IP-Address" {
+  value = docker_container.nodered_container.network_data[0].ip_address
+  description = "The ip addressd of the container."
+}
+
+output "container-name" {
+  value = docker_container.nodered_container.name
+  description = "The name of the container."
+}
