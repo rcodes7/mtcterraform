69b46038baec9c9846ab23eea27b4a3ec8c4e514
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-05 14:45:46 -0600

    move default value to tfvars

--- terraform-docker/variables.tf
+++ terraform-docker/variables.tf
@@ -1,7 +1,5 @@
 variable "ext_port" {
   type = number
-  default = 1880
-
   validation {
     condition = var.ext_port <= 65535 && var.ext_port > 0
     error_message = "The external port must be in the valid port range 0 - 65535."
