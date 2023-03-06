d1727bc1dc8d58677ab6166472d656b8ca4d8178
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-05 22:48:31 -0600

    use local values to create the number of containers as there are ports declared

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -27,19 +27,19 @@ resource "docker_image" "nodered_image" {
 
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "random" {
-  count = var.container_count
+  count = local.container_count
   length = 4
   special = false
   upper = false
 }
 
 resource "docker_container" "nodered_container" {
-  count = var.container_count #https://developer.hashicorp.com/terraform/language/meta-arguments/count
+  count = local.container_count #https://developer.hashicorp.com/terraform/language/meta-arguments/count
   name = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.image_id
   ports {
     internal = var.int_port
-    external = var.ext_port
+    external = var.ext_port[count.index]
   }
   volumes {
     container_path = "/data"
--- terraform-docker/variables.tf
+++ terraform-docker/variables.tf
@@ -1,9 +1,9 @@
 variable "ext_port" {
-  type = number
-  validation {
-    condition = var.ext_port <= 65535 && var.ext_port > 0
-    error_message = "The external port must be in the valid port range 0 - 65535."
-  }
+  type = list(number)
+  # validation {
+  #   condition = var.ext_port <= 65535 && var.ext_port > 0
+  #   error_message = "The external port must be in the valid port range 0 - 65535."
+  # }
 }
 
 variable "int_port" {
@@ -16,7 +16,6 @@ variable "int_port" {
   }
 }
 
-variable "container_count" {
-  type = number
-  default = 1
+locals {
+  container_count = length(var.ext_port)
 }
