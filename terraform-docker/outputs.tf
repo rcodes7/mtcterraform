5e3fedb7d9f0d0d342f0a0298fdd46d0421cbb4d
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-07 22:10:41 -0600

    add outputs for container module

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -1,16 +1,16 @@
-# #View them with (name must not contain spaces):
-# #- terraform output
-# #- terraform console > provide path of attribute youd like to see
-# #- terraform show | grep ....
+#View them with (name must not contain spaces):
+#- terraform output
+#- terraform console > provide path of attribute youd like to see
+#- terraform show | grep ....
 
-# output "ip-address" {
-#   #  use for expression https://developer.hashicorp.com/terraform/language/expressions/for
-#   value = [for container in docker_container.nodered_container[*] : join(":", container.network_data[*].ip_address, container.ports[*].external)]
-#   description = "The ip address and external port of the container."
-# }
+output "ip-address" {
+  #  use for expression https://developer.hashicorp.com/terraform/language/expressions/for
+  value = flatten(module.container[*].ip-address)
+  description = "The ip address and external port of the container."
+}
 
-# output "container-names" {
-#   # splat expressions https://developer.hashicorp.com/terraform/language/expressions/splat
-#   value = docker_container.nodered_container[*].name
-#   description = "The name of the container."
-# }
+output "container-names" {
+  # splat expressions https://developer.hashicorp.com/terraform/language/expressions/splat
+  value = module.container[*].container-names
+  description = "The name of the container."
+}
