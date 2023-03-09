7538d7c24fbdb4f011a7e7980786b51f6f4c337c
raul <123352622+rcodes7@users.noreply.github.com>
2023-03-08 22:05:31 -0600

    create backup of volumes on destroy

--- terraform-docker/container/main.tf
+++ terraform-docker/container/main.tf
@@ -15,6 +15,16 @@ resource "docker_container" "app_container" {
 resource "docker_volume" "container_volume" {
   count = var.count_in
   name = "${var.name_in}-${random_string.random[count.index].result}-volume"
+  provisioner "local-exec" {
+    when = destroy
+    command = "mkdir ${path.cwd}/../backup/"
+    on_failure = continue
+  }
+  provisioner "local-exec" {
+    when = destroy
+    command = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
+    on_failure = continue
+  }
 }
 
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
